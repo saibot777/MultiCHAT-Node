6ca563fc7fa2045356959d5d4532d73c0a3df41d
saibot777 <stefantrajkovic777@gmail.com>
2018-03-05 10:17:23 +0100

    routes implemented

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 'use strict';
 const express = require('express');
 const app = express();
+const chatCat = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+app.use('/', chatCat.router);
 
 app.get('/', (req, res, next) => {
     res.render('login');
