f51510c20ab15a6defa03c24265a5b7fe0e94209
saibot777 <stefantrajkovic777@gmail.com>
2018-03-07 11:37:24 +0100

    addded host variable for socket integration

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const h = require('../helpers');
 const passport = require('passport');
+const config = require('../config');
 
 module.exports = () => {
     let routes = {
@@ -10,12 +11,14 @@ module.exports = () => {
             },
             '/rooms': [h.isAuthenticated, (req, res, next) => {
                 res.render('rooms', {
-                    user: req.user
+                    user: req.user,
+                    host: config.host
                 });
             }],
             '/chat': [h.isAuthenticated, (req, res, next) => {
                 res.render('chatroom', {
-                    user: req.user
+                    user: req.user,
+                    host: config.host
                 });
             }],
             '/auth/facebook': passport.authenticate('facebook'),
