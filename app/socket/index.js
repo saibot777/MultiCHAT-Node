bae56451a385b59b3bca44c892a2dc2d1607b535
saibot777 <stefantrajkovic777@gmail.com>
2018-03-08 09:53:01 +0100

    rendering in rooms UI

--- app/socket/index.js
+++ app/socket/index.js
@@ -3,7 +3,21 @@
 module.exports = (io, app) => {
     let allrooms = app.locals.chatrooms;
 
+    allrooms.push({
+        room: 'Good Food',
+        roomID: '0001',
+        users: []
+    });
+
+    allrooms.push({
+        room: 'Cloud Computing',
+        roomID: '0002',
+        users: []
+    });
+
     io.of('/roomslist').on('connection', socket => {
-        console.log('Socket.io connected to client!');
+        socket.on('getChatrooms', () => {
+            socket.emit('chatRoomsList', JSON.stringify(allrooms));
+        });
     });
 }
